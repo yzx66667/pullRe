4c7a096eab7dea693e5ef607185e077d94cd099d
尹志祥 <13602547976@163.com>
2025-08-22 16:31:03 +0800

    feat: 增加注释

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
+// 全局
 for (var i = 0; i < 10; i++) {
   setTimeout(() => {
     console.log(i);
   }, 1000);
 }
 
+// 局部
 for (let i = 0; i < 10; i++) {
   setTimeout(() => {
     console.log(i);
   }, 1000);
 }
 
+// 闭包
 for (var i = 0; i < 10; i++) {
   (function (i) {
     setTimeout(() => {
