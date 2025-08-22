c388a8c81f0351ba7a3ecdd28febe6dfe2992513
尹志祥 <13602547976@163.com>
2025-08-22 16:26:47 +0800

    feat: first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+for (var i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+}
+
+for (let i = 0; i < 10; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+}
+
+for (var i = 0; i < 10; i++) {
+  (function (i) {
+    setTimeout(() => {
+      console.log(i);
+    }, 1000);
+  })(i);
+}
